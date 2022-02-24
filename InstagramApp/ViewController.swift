72c6e80c5fe972e8fa074090559bc37db41f4de3
farukmeral25 <farukmeral25@gmail.com>
2022-02-24 20:10:28 +0300

    Firebase connection was made with the application. User registration has been done.

--- InstagramApp/ViewController.swift
+++ InstagramApp/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Firebase
+
 
 class ViewController: UIViewController {
     
@@ -25,16 +27,18 @@ class ViewController: UIViewController {
         textField.backgroundColor = UIColor(white: 0, alpha: 0.05)
         textField.borderStyle = .roundedRect
         textField.font = UIFont.systemFont(ofSize: 16)
+        textField.addTarget(self, action: #selector(whenDataChanges), for: .editingChanged)
         return textField
     }()
     
     let textFieldUserName : UITextField = {
        let textField = UITextField()
-        textField.placeholder = "KUllanıcı adınızı giriniz."
+        textField.placeholder = "Kullanıcı adınızı giriniz."
         //textField.translatesAutoresizingMaskIntoConstraints = false
         textField.backgroundColor = UIColor(white: 0, alpha: 0.05)
         textField.borderStyle = .roundedRect
         textField.font = UIFont.systemFont(ofSize: 16)
+        textField.addTarget(self, action: #selector(whenDataChanges), for: .editingChanged)
         return textField
     }()
     
@@ -46,10 +50,11 @@ class ViewController: UIViewController {
         textField.backgroundColor = UIColor(white: 0, alpha: 0.05)
         textField.borderStyle = .roundedRect
         textField.font = UIFont.systemFont(ofSize: 16)
+        textField.addTarget(self, action: #selector(whenDataChanges), for: .editingChanged)
         return textField
     }()
     
-    let buttonSignIn : UIButton = {
+    let buttonSignUp : UIButton = {
        let button = UIButton()
         button.setTitle("Kayıt Ol ", for: .normal)
         //button.backgroundColor = UIColor(red: 150/255, green: 205/255, blue: 245/255, alpha: 1)
@@ -57,8 +62,13 @@ class ViewController: UIViewController {
         button.layer.cornerRadius = 6
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
         button.setTitleColor(.white, for: .normal)
+        ///Butona onPressed methodu ekleme
+        button.addTarget(self, action: #selector(buttonSignUpPressed), for: .touchUpInside)
+        button.isEnabled = false
         return button
     }()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -81,7 +91,7 @@ class ViewController: UIViewController {
     }
     
     fileprivate func createInputField(){
-        let stackView = UIStackView(arrangedSubviews: [textFieldEmail,textFieldUserName,textFieldPassword,buttonSignIn])
+        let stackView = UIStackView(arrangedSubviews: [textFieldEmail,textFieldUserName,textFieldPassword,buttonSignUp])
         //stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
         stackView.distribution = .fillEqually
@@ -99,10 +109,50 @@ class ViewController: UIViewController {
             //stackView.heightAnchor.constraint(equalToConstant: 230)
         ])
         */
+    }
+    
+    @objc fileprivate func buttonSignUpPressed(){
+        //let email = "deneme3@gmail.com"
+        //let password  = "123456"
+        
+        guard let email = textFieldEmail.text else {
+            return
+        }
+        
+        guard let password = textFieldPassword.text else {
+            return
+        }
+        
+        guard let userName = textFieldUserName.text else {
+            return
+        }
         
         
+        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+            if let error = error {
+                print("Kullanıcı kayıt olurken hata meydana geldi.")
+                return
+            }
+            
+            print("Kullanıcı kaydı başarıyla gerçekleşti.")
+            print("\(userName) kullanıcı adlı User ID : \(result?.user.uid)")
+            self.textFieldEmail.text = ""
+            self.textFieldPassword.text = ""
+            self.textFieldUserName.text = ""
+            
+        }
+    }
+    
+    @objc fileprivate func whenDataChanges(){
+        let isFormSuccess = (textFieldEmail.text?.count ?? 0) > 0 && (textFieldUserName.text?.count ?? 0) > 0 &&  (textFieldPassword.text?.count ?? 0) > 0 && ((textFieldEmail.text?.contains("@") ?? false))
         
-        
+        if isFormSuccess {
+            buttonSignUp.isEnabled = true
+            buttonSignUp.backgroundColor = UIColor.convertRGBA(red: 20, green: 155, blue: 235)
+        }else {
+            buttonSignUp.isEnabled = false
+            buttonSignUp.backgroundColor = UIColor.convertRGBA(red: 150, green: 205, blue: 245)
+        }
     }
 
 
