8a38952c71775ad9984c3273a290d44b20d5bb33
farukmeral25 <farukmeral25@gmail.com>
2022-03-04 15:25:50 +0300

    Creating a new tab and view.

--- InstagramApp/Controllers/Home/HomeTabBarController.swift
+++ InstagramApp/Controllers/Home/HomeTabBarController.swift
@@ -33,16 +33,38 @@ class HomeTabBarController: UITabBarController {
     }
     
     func createView (){
+        let homeNavigationController = createNavigationController(image: #imageLiteral(resourceName: "Ana_Ekran_Secili_Degil"),selectedImage: #imageLiteral(resourceName: "Ana_Ekran_Secili"), rootViewController: ProfileViewController(collectionViewLayout: UICollectionViewFlowLayout()))
+        let searchNavigationController = createNavigationController(image: #imageLiteral(resourceName: "Ara_Secili_Degil"),selectedImage: #imageLiteral(resourceName: "Ara_Secili"))
+        let addNavigationController = createNavigationController(image: #imageLiteral(resourceName: "Ekle_Secili_Degil"),selectedImage: #imageLiteral(resourceName: "Ekle_Secili_Degil"))
+        let likeNavigationController = createNavigationController(image: #imageLiteral(resourceName: "Begeni_Secili_Degil"),selectedImage: #imageLiteral(resourceName: "Begeni_Secili"))
+        
         let layout = UICollectionViewFlowLayout()
         let profileViewController = ProfileViewController(collectionViewLayout: layout)
-        let navigationController = UINavigationController(rootViewController: profileViewController)
-        navigationController.tabBarItem.image = #imageLiteral(resourceName: "Profil")
-        navigationController.tabBarItem.selectedImage = #imageLiteral(resourceName: "Profil_Secili")
+        let profileNavigationController = UINavigationController(rootViewController: profileViewController)
+        profileNavigationController.tabBarItem.image = #imageLiteral(resourceName: "Profil")
+        profileNavigationController.tabBarItem.selectedImage = #imageLiteral(resourceName: "Profil_Secili")
         tabBar.tintColor = .black
         
-        viewControllers = [navigationController,UIViewController()]
+        //Tabbar da basıldığında gidilecek viewlar
+        viewControllers = [homeNavigationController,searchNavigationController,addNavigationController,likeNavigationController,profileNavigationController]
+        
+        guard let items = tabBar.items else {
+             return
+        }
+        //Tab Bar arka plan renklendirme
+        //tabBar.backgroundColor = .red
+        //Tab ile iconlar arasına boşluk verme
+        for item in items {
+            item.imageInsets = UIEdgeInsets(top: 5, left: 0, bottom: -5, right: 0)
+        }
     }
 
-    
+    fileprivate func createNavigationController(image:UIImage, selectedImage:UIImage,rootViewController: UIViewController = UIViewController()) -> UINavigationController {
+        let rootViewController = rootViewController
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        navigationController.tabBarItem.image = image
+        navigationController.tabBarItem.selectedImage = selectedImage
+        return navigationController
+    }
 
 }
